9cdd5a14ef78e52ffc73c73bbf930292d42b8a1b
mumtozvalijonov <mumtozvalijonov@gmail.com>
2026-06-07 23:29:40 +0200

    test: add draft pubsub methods for fake repository

--- backend/internal/core/service/fake_cache_repository_test.go
+++ backend/internal/core/service/fake_cache_repository_test.go
@@ -2,7 +2,9 @@ package service_test
 
 import (
 	"context"
+	"fmt"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/mumtozvalijonov/weather/internal/core/domain"
@@ -10,10 +12,42 @@ import (
 	"github.com/mumtozvalijonov/weather/internal/core/service"
 )
 
+var nextID atomic.Uint64
+
+type fakePubSub struct {
+	name        string
+	pubChannel  chan string
+	subChannels map[uint64]chan string
+	running     atomic.Bool
+}
+
+func newFakePubSub(name string) *fakePubSub {
+	return &fakePubSub{
+		name:        name,
+		pubChannel:  make(chan string, 1),
+		subChannels: make(map[uint64]chan string),
+	}
+}
+
+func (ps *fakePubSub) run() {
+	if !ps.running.CompareAndSwap(false, true) {
+		return
+	}
+
+	for {
+		msg := <-ps.pubChannel
+		for _, ch := range ps.subChannels {
+			ch <- msg // TODO: account for when channel is closed
+		}
+	}
+}
+
 type fakeWeatherRepository struct {
-	mu   sync.Mutex
-	data map[string]*domain.Forecast
-	geo  map[string]map[string]fakeGeoLocation
+	mu      sync.Mutex
+	data    map[string]*domain.Forecast
+	geo     map[string]map[string]fakeGeoLocation
+	locks   map[string]*sync.Mutex
+	pubSubs map[string]*fakePubSub
 }
 
 type fakeGeoLocation struct {
@@ -116,14 +150,92 @@ func (f *fakeWeatherRepository) Close() error {
 }
 
 func (f *fakeWeatherRepository) TryLock(ctx context.Context, key string, ttl time.Duration) (bool, error) {
-	panic("")
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	if f.locks == nil {
+		f.locks = make(map[string]*sync.Mutex)
+	}
+	if _, ok := f.locks[key]; !ok {
+		f.locks[key] = &sync.Mutex{}
+	}
+	ok := f.locks[key].TryLock()
+	return ok, nil
 }
+
 func (f *fakeWeatherRepository) Unlock(ctx context.Context, key string) error {
-	panic("")
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	if f.locks == nil {
+		return nil
+	}
+
+	if c, ok := f.locks[key]; ok {
+		c.Unlock()
+		delete(f.locks, key)
+		return nil
+	}
+	return nil
 }
+
 func (f *fakeWeatherRepository) Subscribe(ctx context.Context, channelName string) (port.CancelFunc, port.NextMessageFunc) {
-	panic("")
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	if f.pubSubs == nil {
+		f.pubSubs = make(map[string]*fakePubSub)
+	}
+
+	if _, ok := f.pubSubs[channelName]; !ok {
+		f.pubSubs[channelName] = newFakePubSub(channelName)
+	}
+
+	pubSub := f.pubSubs[channelName]
+	subCh := make(chan string)
+	subId := nextID.Add(1)
+	pubSub.subChannels[subId] = subCh
+
+	chanCtx, cancel := context.WithCancel(ctx)
+
+	cancelFunc := func() error {
+		delete(pubSub.subChannels, subId)
+		close(subCh)
+		cancel()
+		return nil
+	}
+
+	nextMessageFunc := func() (string, bool) {
+		select {
+		case msg, ok := <-subCh:
+			if ok {
+				return msg, true
+			}
+		case <-chanCtx.Done():
+		}
+		return "", false
+	}
+
+	return cancelFunc, nextMessageFunc
 }
+
 func (f *fakeWeatherRepository) Publish(ctx context.Context, channelName string, message any) error {
-	panic("")
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	if f.pubSubs == nil {
+		f.pubSubs = make(map[string]*fakePubSub)
+	}
+
+	if _, ok := f.pubSubs[channelName]; !ok {
+		f.pubSubs[channelName] = newFakePubSub(channelName)
+	}
+
+	go f.pubSubs[channelName].run()
+
+	select {
+	case f.pubSubs[channelName].pubChannel <- fmt.Sprintf("%v", message):
+	case <-ctx.Done():
+	}
+	return nil
 }
